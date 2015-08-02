914bcc10c0eac97d2b96583322621389a53aaa35
Derek <derek.kier@gmail.com>
2015-08-01 22:07:42 -0700

    Add js dir.

---
+++ js/prototype/string.js
@@ -0,0 +1,9 @@
+/*
+Author: Derek Kier
+Description: Adding methods and propertys to the String prototype
+Version: 1.0
+Note: In case JavaScript ends up creating the properties or methods I create, I prefix the method or function with util  
+*/
+
+string.prototype.utilLength = 2;
+
